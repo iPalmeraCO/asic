54aa1c435e52f82b805f2640cfe4c912e2804f20
Julián Andrés Escobar Herrera <julian.escobar@ipalmera.co>
2016-07-14 17:22:48 -0500

    *Se Crea template de productos y unidades de  Apoyo *Se Suben los Productos

--- wp-content/themes/twentythirteen/content.php
+++ wp-content/themes/twentythirteen/content.php
@@ -8,17 +8,18 @@
  * @subpackage Twenty_Thirteen
  * @since Twenty Thirteen 1.0
  */
+$feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
 ?>
 
 <?php
 if ( function_exists('yoast_breadcrumb') ) {
 yoast_breadcrumb('<p id="breadcrumbs">','</p>');
 }
-if (get_post_type( get_the_ID() ) == 'servicios' ){?>
+if (get_post_type( get_the_ID() ) == 'servicios' || get_post_type( get_the_ID() ) == 'productos' ){?>
 <div class="container">
 	<div class="row">	
 		<div class="imgservicios">
-			<?php the_post_thumbnail(); ?>
+			<img src="<?php echo $feat_image; ?>">
 		</div>		
 	</div>
 <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">	
@@ -28,7 +29,7 @@ if (get_post_type( get_the_ID() ) == 'servicios' ){?>
                 <p id="text7">Otros Servicios</p>
                  <div class="servicios">
                  	<?php
-                 	$args = array('category_name'=>'servicios','post_type'=>'servicios','post__not_in'=>array(get_the_ID()));
+                 	$args = array('category_name'=>'servicios','post_type'=>'servicios','post__not_in'=>array(get_the_ID()),'orderby'=>'date','order'=>'ASC');
                  	$the_query = new WP_Query( $args );
 
 // The Loop
@@ -46,6 +47,48 @@ if ( $the_query->have_posts() ) {
             	<a href="<?php echo get_permalink(); ?>"><img src="<?php echo $rutimg; ?>" class="img-responsive image" alt="Imagen responsive" ></a>
             </div>
             <div class="col-md-8 text">
+            	<h6> <?php echo  $text; ?> </h6>
+            </div>
+            
+        </div>
+		<?php
+		
+	}
+	
+	/* Restore original Post Data */
+	wp_reset_postdata();
+}
+
+?>
+
+               
+
+ </div>
+ </div>
+
+ <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                <p id="text7">Productos</p>
+                 <div class="productos">
+                 	<?php
+                 	$args = array('category_name'=>'productos','post_type'=>'productos','post__not_in'=>array(get_the_ID()), 'orderby'=>'date','order'=>'ASC');
+                 	$the_query = new WP_Query( $args );
+
+// The Loop
+if ( $the_query->have_posts() ) {
+	
+	while ( $the_query->have_posts() ) {
+		$the_query->the_post();		
+		$text =  get_field('textominiatura',get_the_ID());
+		$img  = get_field('miniatura',get_the_ID());
+		$rutimg =$img["url"];
+
+		?>
+		 <div class="contentservicios">
+		 	<div class="col-md-4 img">
+            	<a href="<?php echo get_permalink(); ?>"><img src="<?php echo $rutimg; ?>" class="img-responsive image" alt="Imagen responsive" ></a>
+            </div>
+            <div class="col-md-8 text">
+            	<h6 class="titprod"> <?php the_title(); ?> </h6>
             	<h6> <?php echo  $text; ?> </h6>
             </div>
             
