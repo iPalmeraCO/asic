f6125fef907a4e278053c54c942f205da53f7a78
Julián Andrés Escobar Herrera <julian.escobar@ipalmera.co>
2016-09-30 18:17:25 -0500

    *Update script sql

--- updateasic.sql
+++ updateasic.sql
@@ -1,6 +1,5 @@
-
-UPDATE wp_posts SET guid = replace(guid, 'http://localhost/asic','http://159.203.108.98/asic');
-
-UPDATE wp_posts SET post_content = replace(post_content, 'http://localhost/asic', 'http://159.203.108.98/asic');
-
-UPDATE wp_postmeta SET meta_value = replace(meta_value,'http://localhost/asic','http://159.203.108.98/asic');
+UPDATE wp_posts SET guid = replace(guid, 'http://localhost/asic','http://172.20.20.105');
+UPDATE wp_posts SET post_content = replace(post_content, 'http://localhost/asic', 'http://172.20.20.105');
+UPDATE wp_postmeta SET meta_value = replace(meta_value,'http://localhost/asic','http://172.20.20.105');
+UPDATE wp_options SET option_value = 'http://172.20.20.105' WHERE option_id = 1;
+UPDATE wp_options SET option_value = 'http://172.20.20.105' WHERE option_id = 2;
