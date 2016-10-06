cc7f6e9d85d0e678e55ab6a3c8ac3980d24a858b
Julián Andrés Escobar Herrera <julian.escobar@ipalmera.co>
2016-10-06 10:06:27 -0500

    *Fix

--- wp-content/themes/twentythirteen/ingreso.php
+++ wp-content/themes/twentythirteen/ingreso.php
@@ -6,7 +6,7 @@ if ($_POST['accion'] == 1){
 	$usuario     = $_POST["logusername"];
 	$contrasena  = $_POST["logpassword"];
 	$data = array();
-	$ingreso=$prueba::autentificar($_POST["logusername"],$_POST["logpassword"]);
+	$ingreso=$prueba::autentificar($usuario,$contrasena);
 			if ($ingreso == true){
 			$data['success'] = true;
 			$_SESSION['usuario']=$usuario;
